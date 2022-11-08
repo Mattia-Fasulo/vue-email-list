4cb41f3bc5678fa64431e465993826363c050795
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-11-08 16:20:25 +0100

    add  mailing list

--- js/script.js
+++ js/script.js
@@ -7,29 +7,37 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            
+            mailingList: [],
         }
     },
     methods: {
-        
+        callApi(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+                    // console.log(res.data.response)
+                    this.mailingList.push(res.data.response)
+                })
+            }
+        }
     },
     computed: {
 
     },
     mounted() {
-
+        console.log('in mounted');
+        this.callApi()
     },
     created(){
-
+        console.log('in created');
     },
     beforeCreate(){
-
+        console.log('in beforeCreate');
     },
     beforeUpdate(){
-        
+        console.log('in beforeUpadate');
     },
     update(){
-
+        console.log('in update');
     }
 }).mount('#app')
 
