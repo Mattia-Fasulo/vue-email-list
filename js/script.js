5b8fed7ee9c02348e144daac65ad4eb6702b1e94
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-11-08 15:32:33 +0100

    add scaffold

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    created(){
+
+    },
+    beforeCreate(){
+
+    },
+    beforeUpdate(){
+        
+    },
+    update(){
+
+    }
+}).mount('#app')
+
+
+
